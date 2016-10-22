c04aa2ddc2f0dadd45dc24c25f8d47d104f83677
YannRS <yannrs31@yahoo.fr>
2016-10-21 23:43:40 -0400

    last version with some optimisations

--- Assignment_3/main_test.py
+++ Assignment_3/main_test.py
@@ -14,16 +14,16 @@ Input:
 """
 def apriori_based(inputFile, threshold, outputFile):
 
-    # Load dataset
+    print 'begin extract'
     data = generateItem(inputFile)
-	
-	# Preprocess data
-    data = sortData(data)
-    
-	# Generate Subset
-    Lk = findItemSet(data, threshold)
-	
-    # Save results
+    print 'old length', len(data)
+    # data = sortData(data)
+    print 'new length', len(data)
+    # print 'data', data
+    print 'second step ', int(NBLINE*0.98)
+    Lk = findItemSet(data, int(NBLINE*0.80))
+    # Lk = findItemSet(data, threshold)
+    print 'third step'
     writeItemsSet(Lk, outputFile)
 
     return Lk
@@ -48,14 +48,18 @@ def findItemSet(data, sup_min):
         # Generate Candidate with a size k
         Ck = generateCandidateFromLk(Lk, k)
         print len(Ck)
+        # print 'Ck', Ck
 
         # Count apparition of itemset on data
         Ck_ = countCandidate(data, Ck)
+        # print 'Ck_', Ck_
 
         # Keep only itemset with a frequency above the limit
-        Lk = removeBelowLimit(Ck_, sup_min)
+        # Lk = filter((lambda x: x > sup_min), Ck)
 
-		# Save previous subset
+        Lk = removeBelowLimit(Ck_, sup_min)
+        # print 'Lk', Lk
+
         Lk_save.append(copy.deepcopy(Lk))
         k += 1
 
@@ -102,7 +106,7 @@ def sortData(data):
 
 """ Extract unique element
 Input:
-    - data: { frozenset(item1, item2, ...): 5, ... }
+    - data: [ frozenset( item1, item2, ...), ....]
 Output:
     - { frozenset( item1, ...):0, ... }
 """
@@ -111,7 +115,7 @@ def extractItem(data):
 
     for line in data:
         items = items.union(line)
-    print 'extractItem', 'Nb items', len(items)
+    print 'extractItem', 'items', len(items)
     return {frozenset([key]): 0 for key in items}
 
 
@@ -124,10 +128,12 @@ Output:
 """
 def generateCandidateFromLk(Lk, nb_items):
     # Extract primitive element of key
+    # print 'generateCandidateFromLk',' Lk', Lk
     prim_elements = frozenset()
     for key in Lk.keys():
         prim_elements = prim_elements.union(key)
 
+    # print 'generateCandidateFromLk ', 'prim_elements', prim_elements
     # Generate all permutation with k elements
     comb = [frozenset(key) for key in itertools.combinations(prim_elements, nb_items)]
 
@@ -143,11 +149,17 @@ Output:
 """
 def countCandidate(data, Ck):
     Lk = {i: 0 for i in Ck}
+    # nbElement = len(Ck[0])
     for setItems in Ck:
         for line in data:
             if setItems <= line:
-                Lk[setItems] += data[line]
+                # Lk[setItems] += data[line]
+                Lk[setItems] += 1
 
+    # for key in Lk:
+    #     Lk[key] /= float(NBLINE)
+
+    # print 'countCandidate', 'k', Lk
     return Lk
 
 
@@ -161,23 +173,35 @@ def removeBelowLimit(list, threshold):
 
 
 def writeItemsSet(Lk, filename):
+
     file = open(filename, "w+")
 
     nbSize = len(Lk)
     for i in range(0, nbSize):
         for s in Lk[i].keys():
+            # file.write(" ".join([str(it) for it in s]) + ' (' + str(Lk[i][s]*NBLINE) + ') \n')
             file.write(" ".join([str(it) for it in s]) + ' (' + str(Lk[i][s]) + ') \n')
 
 
 
+
+""" From frequent itemsets generate rules
+"""
+def generateRules():
+    return
+
+
 if __name__ == '__main__':
+    # data = ['A', 'B', 'C', 'D', 'E']
+    # print generateCandidateFromLk(data, 3)
     start_time = time.clock()
 
-	## Threshold but as 80% of the total number of line
-    print apriori_based("Data/chess.dat", 2556, "Data/chess_output.dat")
+    # print apriori_based("Data/chess.dat", 0.8, "Data/chess_output2.dat")
+    print apriori_based("Data/chess.dat", 2556, "Data/chess_output3.dat")
     # print apriori_based("Data/chess_custom.dat", 2556, "Data/chess_custom_output3.dat")
-	
-    # print apriori_based("Data/mushroom.dat", 3000, "Data/mushroom_output.dat")
-    # print apriori_based("Data/pumsb.dat", 46593, "Data/pumsb_output.dat")
+    print apriori_based("Data/mushroom.dat", 3000, "Data/mushroom_output.dat")
+    # print apriori_based("Data/mushroom_custom.dat", 3000, "Data/mushroom_custom_output.dat")
+    # print apriori_based("Data/pumsb.dat", 3000, "Data/pumsb_output.dat")
 
+    # print("--- %s seconds ---" % (time.clock() - start_time))
     print("%s seconds" % (time.clock() - start_time))
